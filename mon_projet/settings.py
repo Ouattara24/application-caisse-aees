a216496d1028a2851df06247af3f66e4684d98e0
Kouegnanran Ouattara <kouegnanrano@gmail.com>
2026-05-14 23:20:29 +0200

    fix redirect loop railway

--- mon_projet/settings.py
+++ mon_projet/settings.py
@@ -158,7 +158,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Security settings for production
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
+    SECURE_SSL_REDIRECT = False
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
